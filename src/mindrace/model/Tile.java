f945ed6b27d602a4e285e69abc4767a9a14d4d73
Unknown <dliberman@itba.edu.ar>
2016-11-01 12:32:47 -0300

    termino Tile

--- src/mindrace/model/Tile.java
+++ src/mindrace/model/Tile.java
@@ -1,20 +1,45 @@
 package mindrace.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author Daniella
  *
  */
 public class Tile {
-	Integer position;
-	Boolean isStart;
-	Set<Player> Players;
+	private Integer position;
+	private Boolean isStart;
+	private Set<Player> players = new HashSet<Player>();
 
-	public Tile(Integer position, Boolean isStart, Set<Player> players) {
+	public Tile(Integer position, Boolean isStart) {
 		super();
 		this.position = position;
 		this.isStart = isStart;
-		Players = players;
+	}
+	
+	public Integer getPosition(){
+		return position;
 	}
 	
 	public Boolean isStart(){
+		return isStart;
+	}
+	
+	public void addPlayer(Player player){
+		players.add(player);
+	}
+	
+	public void removePlayer(Player player){
+		players.remove(player);
+	}
+	
+	public boolean equals(Object obj) {
+		if (obj == null)
+			return false;
+		if (!(obj.getClass().equals(this.getClass())))
+			return false;
+		Tile tile = (Tile)obj;
+		return this.position.equals(tile.position);
+	}
 }

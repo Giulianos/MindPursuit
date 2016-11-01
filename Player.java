2ac8bcb323fb9053c573a558ef064a8d587b7b76
fsammart <frperez@itba.edu.ar>
2016-11-01 11:54:34 -0300

    subo las clases (Game, NewTurn , Player)

---
+++ Player.java
@@ -0,0 +1,41 @@
+package mindRace;
+
+import java.util.Locale.Category;
+import java.util.Set;
+
+public class Player{
+	private String name;
+	private Set<Category> tokens;
+	private Tile tile;
+	public Player(String name){
+		this.name = name;
+		this.tokens = new HashSet<Category>();
+		this.tile = new Tile();
+	}
+	public String getName(){
+		return name;
+	}
+	public addToken(Category category){
+		if(tokens.contains(category)){
+			throw new IllegalArgumentException("Ya tiene ese token");
+		}
+		tokens.add(category);
+	}
+	public void move(int quantity){
+		if(quantity<0){
+			throw new IllegalArgumentException("quantity must be non negative");
+		}
+		for (int i=quantity; i>0;i--){
+			tile=tile.next;
+		}
+	}
+	@Override
+	public boolean equals(Object obj) {
+		return this.name.equals(obj.toString());
+	}
+	@Override
+	public int hashCode() {
+		return this.name.hashCode();
+	}
+
+}

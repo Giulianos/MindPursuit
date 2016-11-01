bd1d0e91915040b386d2689f640dab222a521b7d
fsammart <frperez@itba.edu.ar>
2016-11-01 19:03:41 -0300

    Situation implementa cloneable

--- src/mindrace/model/Situation.java
+++ src/mindrace/model/Situation.java
@@ -1,12 +1,13 @@
 package mindrace.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
  * @author Giuliano
  *
  */
-public class Situation {
+public class Situation implements Cloneable{
 	private Set<Player> players;
 	private Player currentPlayer;
 	private Board currentBoard;
@@ -19,6 +20,13 @@ public class Situation {
 	
 	public Player getCurrentPlayer() {
 		return currentPlayer;
+	}
+	public Situation clone(){
+		Set<Player> copyPlayers=new HashSet<Player>();
+		for(Player p: this.players){
+			copyPlayers.add(p.clone());
+		}
+		Situation clonedSituation= new Situation(copyPlayers,currentPlayer.clone(),currentBoard);
 	}
 	
 }

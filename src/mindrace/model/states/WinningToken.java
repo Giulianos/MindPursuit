9191492c623a82cae055e524127d912a088b4a6e
Unknown <dliberman@itba.edu.ar>
2016-11-04 00:36:53 -0300

    termino WinningCategory

--- src/mindrace/model/states/WinningToken.java
+++ src/mindrace/model/states/WinningToken.java
@@ -1,7 +1,11 @@
 package mindrace.model.states;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import mindrace.model.Category;
 import mindrace.model.Situation;
+import mindrace.model.TileWithCategory;
 import mindrace.model.states.ModifierState;
 
 /**
@@ -10,25 +14,57 @@ import mindrace.model.states.ModifierState;
  */
 public class WinningToken extends ModifierState {
 	private Category winningCategory;
+	private Set<Category> possibleTokens;
+
 	/**
 	 * @see mindrace.model.states.ModifierState#initialize()
 	 */
 	@Override
 	public void initialize() {
-		(this.getSituation().getCurrentPlayer()).addToken(winningCategory);
+		if (this.getPreviousState().equals(Asking.class)){
+			TileWithCategory auxTile = (TileWithCategory) this.getSituation().getCurrentPlayer().getTile(); 
+			winningCategory = auxTile.getCategory();  
+			(this.getSituation().getCurrentPlayer()).addToken(winningCategory);
+		}
+		/**
+		 * si no viene de asking, entonces viene de moving.
+		 */
+		else{
+			/**
+			 * @see http://stackoverflow.com/a/27980614
+			 */
+			
+			possibleTokens = new HashSet<Category>(); 
+			for(Category c: Category.values()){
+				possibleTokens.add(c);
+			}
+			for(Category c: this.getSituation().getCurrentPlayer().getTokens()){
+				possibleTokens.remove(c);
+			}
+		}
 	}
 
+	public Set<Category> getPossibleTokens(){
+		return possibleTokens;
+	}
+	
+	public void setChosenCategory(Category category){
+		winningCategory = category;
+	}
+	
 	/**
 	 * @see mindrace.model.states.ModifierState#terminate()
 	 */
 	@Override
 	public State terminate() {
-		if(this.getPreviousState().equals(Moving.class)){
+		if(this.getPreviousState().equals(Asking.class)){
+			return new Moving();
+		} else if (this.getSituation().getCurrentPlayer().getTile().getClass().equals(TileWithCategory.class)){
+			(this.getSituation().getCurrentPlayer()).addToken(winningCategory);
+			return new Asking();
+		} else {
+			(this.getSituation().getCurrentPlayer()).addToken(winningCategory);
 			return new ChoosingCategory();
 		}
-		return new Moving();
 	}
-
-
-
 }
